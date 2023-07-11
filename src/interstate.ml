30045e773f9bbbfe8932304998551481ef5cdaaa
Anoushka <anoushka.m.arora@gmail.com>
2023-07-11 20:59:36 +0000

    Graph

--- src/interstate.ml
+++ src/interstate.ml
@@ -1,4 +1,76 @@
 open! Core
+open! Core
+module City = String
+
+(* We separate out the [Network] module to represent our social network in
+   OCaml types. *)
+module Network = struct
+  (* We can represent our social network graph as a set of connections, where
+     a connection represents a friendship between two people. *)
+  module Connection = struct
+    module T = struct
+      type t = City.t * City.t [@@deriving compare, sexp]
+    end
+
+    (* This funky syntax is necessary to implement sets of [Connection.t]s.
+       This is needed to defined our [Network.t] type later. Using this
+       [Comparable.Make] functor also gives us immutable maps, which might
+       come in handy later. *)
+    include Comparable.Make (T)
+
+    let of_string s =
+      String.split s ~on:',' |> List.map ~f:(fun elem -> City.of_string elem)
+    ;;
+  end
+
+  type t = Connection.Set.t [@@deriving sexp_of]
+
+  let of_file input_file =
+    let connections =
+      In_channel.read_lines (File_path.to_string input_file)
+      |> List.concat_map ~f:(fun s ->
+           match Connection.of_string s with
+           | Some (a, b) ->
+             (* Friendships are mutual; a connection between a and b means we
+                should also consider the connection between b and a. *)
+             [ a, b; b, a ]
+           | None ->
+             printf
+               "ERROR: Could not parse line as connection; dropping. %s\n"
+               s;
+             [])
+    in
+    Connection.Set.of_list connections
+  ;;
+end
+
+(* In order to visualize the social network, we use the ocamlgraph library to
+   create a [Graph] structure whose vertices are of type [Person.t].
+
+   The ocamlgraph library exposes lots of different ways to construct
+   different types of graphs. Take a look at
+   https://github.com/backtracking/ocamlgraph/blob/master/src/imperative.mli
+   for documentation on other types of graphs exposed by this API. *)
+module G = Graph.Imperative.Graph.Concrete (City)
+
+(* We extend our [Graph] structure with the [Dot] API so that we can easily
+   render constructed graphs. Documentation about this API can be found here:
+   https://github.com/backtracking/ocamlgraph/blob/master/src/dot.mli *)
+module Dot = Graph.Graphviz.Dot (struct
+  include G
+
+  (* These functions can be changed to tweak the appearance of the generated
+     graph. Check out the ocamlgraph graphviz API
+     (https://github.com/backtracking/ocamlgraph/blob/master/src/graphviz.mli)
+     for examples of what values can be set here. *)
+  let edge_attributes _ = [ `Dir `None ]
+  let default_edge_attributes _ = []
+  let get_subgraph _ = None
+  let vertex_attributes v = [ `Shape `Box; `Label v; `Fillcolor 1000 ]
+  let vertex_name v = v
+  let default_vertex_attributes _ = []
+  let graph_attributes _ = []
+end)
 
 let load_command =
   let open Command.Let_syntax in
@@ -9,25 +81,30 @@ let load_command =
         flag
           "input"
           (required File_path.arg_type)
-          ~doc:"FILE a file listing interstates and the cities they go through"
+          ~doc:
+            "FILE a file listing interstates and the cities they go through"
       in
       fun () ->
-        ignore (input_file : File_path.t);
-        failwith "TODO"]
+        let network = Network.of_file input_file in
+        (* This special syntax can be used to easily sexp-serialize values
+           (whose types have [sexp_of_t] implemented). *)
+        printf !"%{sexp: Network.t}\n" network]
 ;;
 
 let visualize_command =
   let open Command.Let_syntax in
   Command.basic
     ~summary:
-      "parse a file listing interstates and generate a graph visualizing the highway \
-       network"
+      "parse a file listing interstates and generate a graph visualizing \
+       the highway network"
     [%map_open
       let input_file =
         flag
           "input"
           (required File_path.arg_type)
-          ~doc:"FILE a file listing all interstates and the cities they go through"
+          ~doc:
+            "FILE a file listing all interstates and the cities they go \
+             through"
       and output_file =
         flag
           "output"
